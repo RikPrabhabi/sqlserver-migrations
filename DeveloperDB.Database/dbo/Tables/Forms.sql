5ef601945cec2bc2ed780bc2283a9884f403f073
RikPrabhabi <rikm@prabhabi.in>
2026-05-13 15:48:44 +0530

    Changed in forms table

--- DeveloperDB.Database/dbo/Tables/Forms.sql
+++ DeveloperDB.Database/dbo/Tables/Forms.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Forms] (
     [ID]   INT           IDENTITY (1, 1) NOT NULL,
     [Name] VARCHAR (100) NOT NULL,
+    [Email] VARCHAR(100) NULL, 
     PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
